14e94517d0b8d80b0eff579dbddb7355ecf4b895
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-20 21:56:19 +0530

    added new web

--- redis.sh
+++ redis.sh
@@ -36,7 +36,7 @@ fi
 echo
 echo "----------------------------------------------------------------------------------------"
 echo -e "INSTALLING REDIS"
-if [ ls "/etc/yum.repos.d/remi*.repo" -eq 0 ]
+if ls "/etc/yum.repos.d/remi*.repo"
 then
   echo -e "${RED}REDIS REPO ALREADY INSTALLED SO SKIPPING"
 else
