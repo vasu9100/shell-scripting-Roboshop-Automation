f557a0e238d5f4c29f20de2fa555f8beee6980f8
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-22 16:30:51 +0530

    robshop added

--- cart.sh
+++ cart.sh
@@ -5,21 +5,31 @@
 # Date: March 20, 2024
 # Version: 1.0
 
-# Define variables
+# Define colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+PINK='\033[0;35m'
 RESET='\033[0m'
+
+# Define variables
 DATE=$(date +'%F-%H-%M-%S')
 USER_ID=$(id -u)
 LOG_FILE="/tmp/$0-$DATE.log"
 
+# Function to print task start messages
+TASK_STARTED() {
+    echo "-------------------------------------------------------------------------------------------"
+    echo -e "${PINK}Task Started: $1${RESET}"
+    echo "-------------------------------------------------------------------------------------------"
+}
+
 # Function to validate commands
 VALIDATE() {
     if [ $1 -eq 0 ]; then
-        echo -e "${YELLOW}$2... ${GREEN}SUCCESS${RESET}"
+        echo -e "${GREEN}SUCCESS: $2${RESET}"
     else
-        echo -e "${YELLOW}$2... ${RED}FAILED${RESET}"
+        echo -e "${RED}FAILED: $2${RESET}"
         exit 1
     fi
 }
@@ -31,77 +41,94 @@ else
     echo -e "${RED}ERROR: You are not a root user. Please switch to the root user.${RESET}"
     exit 1
 fi
+
 echo
-echo "----------------------------------------------------------------------------------------"
-echo -e "${GREEN}DISABLING NODE-JS AND ENABLING LATEST VERSION ${RESET}"
+
+TASK_STARTED "Disabling Node.js and enabling the latest version"
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "DISABLED NODE-JS"
-echo
+VALIDATE $? "Disabled Node.js"
 dnf module enable nodejs:18 -y &>>$LOG_FILE
-VALIDATE $? "ENABLED NODE-JS"
-echo "-----------------------------------------------------------------------------------------"
-echo -e "${YELLOW}$0 is checking Node-js whether installed or not in the system ${RESET}"
+VALIDATE $? "Enabled Node.js"
+
+echo
+
+echo -e "${YELLOW}Checking if Node.js is installed${RESET}"
 which node
 if [ $? -eq 0 ]
 then
-    echo -e "${GREEN}NODE JS ALREADY INSTALLED ${GREEN}"
+    echo -e "${GREEN}SUCCESS: Node.js is already installed.${RESET}"
 else
-    echo -e "${GREEN}NODE JS 18 INSTALLING"
+    TASK_STARTED "Installing Node.js 18"
     dnf install nodejs -y &>>$LOG_FILE
-    VALIDATE $? "NODEJS-18 INSTALLATION"
+    VALIDATE $? "Node.js 18 Installation"
 fi
-echo "-------------------------------------------------------------------------------------------"
+
 echo
+
 id roboshop &>>$LOG_FILE
 if [ $? -eq 0 ]
 then
-    echo -e "${GREEN}ROBO-SHOP USER ALREADY AVAILABLE So SKIIPING USER CREATION ${GREEN}"
+    echo -e "${GREEN}SUCCESS: Robo-shop user already exists. Skipping user creation.${RESET}"
 else
-    echo -e "${GREEN}ROBO-SHOP USER CREATION STARTED"
+    TASK_STARTED "Creating Robo-shop user"
     useradd roboshop &>>$LOG_FILE
-    VALIDATE $? "ROBO-SHOP USER CREATION PART"
+    VALIDATE $? "Robo-shop user creation"
 fi
-echo "--------------------------------------------------------------------------------------------"
+
 echo
+
 if [ -d /app ]
 then
-    echo -e "{$RED}/app FOLDER ALREADY EXISTED SO SKIPPING FOLDER CREATION $RESET"
+    echo -e "${RED}SUCCESS: /app folder already exists. Skipping folder creation.${RESET}"
 else
-    echo -e "${YELLOW}/app FOLDER CREATION STARTED $RESET"
+    TASK_STARTED "Creating /app folder"
     mkdir -p /app &>>$LOG_FILE
-    VALIDATE $? "APP FOLDER CREATION"
+    VALIDATE $? "/app folder creation"
 fi
-echo "-------------------------------------------------------------------------------------------"
+
 echo
-echo -e "${GREEN}DOWNLOADING THE APPLICATION CODE FROM INTERNET ${RESET}"
+
+echo -e "${GREEN}Downloading the application code from the internet.${RESET}"
 curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOG_FILE
-VALIDATE $? "APP CODE DOWALOADING"
+VALIDATE $? "Application code downloading"
+
 echo
-echo -e "${GREEN}UNZIPPING THE DOWNLOAD APP CODE"
+
+TASK_STARTED "Unzipping the downloaded application code"
 cd /app
 pwd
 unzip -o /tmp/cart.zip &>>$LOG_FILE
-VALIDATE $? "UNZIPPED CODE INTO /APP"
+VALIDATE $? "Unzipped code into /app"
+
 echo
-echo -e "${GREEN}NPM INSTALLATION STARTED $RESET"
+
+echo -e "${GREEN}NPM installation started.${RESET}"
 npm install
-VALIDATE $? "NPM INSTALLATION"
-echo "-----------------------------------------------------------------------------------------"
+VALIDATE $? "NPM installation"
+
 echo
+
 systemctl daemon-reload
-VALIDATE $? "DAEMON RELOADED"
+VALIDATE $? "Daemon reloaded"
+
 echo
+
+TASK_STARTED "Copying cart.service to /etc/systemd/system"
 cp /home/centos/shell-scripting-Roboshop-Automation/cart.service /etc/systemd/system/cart.service
-VALIDATE $? "Catalogue.service Copying"
+VALIDATE $? "Copied cart.service"
+
 systemctl enable cart
-VALIDATE $? "ENABLED CART SERVICE"
+VALIDATE $? "Enabled cart service"
+
 echo
+
 systemctl start cart
-VALIDATE $? "STARTED CART SERVICE"
-echo "-----------------------------------------------------------------------------------------------"
+VALIDATE $? "Started cart service"
+
+echo
 
 systemctl restart cart
-VALIDATE $? "CART RESTARTED"
+VALIDATE $? "Restarted cart service"
 
 echo "------------------------------ THE-END--------------------------------------"
 echo "SCRIPT END TIME: $0-$DATE"
