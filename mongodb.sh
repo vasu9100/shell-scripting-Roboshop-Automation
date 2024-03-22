55aee867a4d7e768b656f1a8168ff32b14a3d27d
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-22 16:26:44 +0530

    robshop added

--- mongodb.sh
+++ mongodb.sh
@@ -44,13 +44,13 @@ fi
 
 echo -e "${YELLOW}SCRIPT EXECUTION START TIME: $DATE${RESET}"
 echo -e "${PINK}MONGO-DB INSTALLATION SCRIPT STARTED TIME: $DATE ${RESET}"
-echo "-------------------------------------------------------------------------"
+
 
 # Setup MongoDB repository
 TASK_STARTED "SETTING UP MONGO REPOSITORY FILE"
 cp /home/centos/shell-scripting-Roboshop-Automation/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 VALIDATE $? "MONGO-REPO FILE COPYING"
-echo "-------------------------------------------------------------------------"
+
 
 # Check if MongoDB is already installed
 TASK_STARTED "VERIFYING WHETHER MONGO-DB IS ALREADY INSTALLED ON THE LINUX SYSTEM OR NOT"
@@ -62,7 +62,7 @@ else
     VALIDATE $? "MONGO-DB INSTALLATION"
 fi
 
-echo "-------------------------------------------------------------------------"
+
 
 # Start and enable MongoDB
 TASK_STARTED "STARTING AND ENABLING MONGO-DB"
@@ -71,21 +71,21 @@ VALIDATE $? "MONGO-DB ENABLED"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "MONGO-DB STARTED"
 
-echo "-------------------------------------------------------------------------"
+
 
 # Update listen address from 127.0.0.1 to 0.0.0.0
 TASK_STARTED "UPDATING LISTEN ADDRESS FROM 127.0.0.1 TO 0.0.0.0"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "UPDATED LISTEN ADDRESS"
 
-echo "-------------------------------------------------------------------------"
+
 
 # Restart MongoDB
 TASK_STARTED "RESTARTING MONGO-DB"
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "MONGO-DB RESTARTED"
 
-echo "-------------------------------------------------------------------------"
+
 
 # Check listener updation
 TASK_STARTED "CHECKING LISTENER UPDATION"
