9f833244bc921ee2f5e35f06127b5c23bb60f985
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-22 17:37:05 +0530

    robshop added

--- payment.sh
+++ payment.sh
@@ -5,7 +5,7 @@
 # Date: March 20, 2024
 # Version: 1.0
 
-# Define variables
+# Define colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -17,79 +17,90 @@ LOG_FILE="/tmp/$0-$DATE.log"
 # Function to validate commands
 VALIDATE() {
     if [ $1 -eq 0 ]; then
-        echo -e "${YELLOW}$2... ${GREEN}SUCCESS${RESET}"
+        echo -e "${GREEN}SUCCESS: $2${RESET}"
     else
-        echo -e "${YELLOW}$2... ${RED}FAILED${RESET}"
+        echo -e "${RED}FAILED: $2${RESET}"
         exit 1
     fi
+}
+# Function to print task start messages
+TASK_STARTED() {
+    echo "-------------------------------------------------------------------------------------------"
+    echo -e "${PINK}Task Started: $1${RESET}\n"
+    echo "-------------------------------------------------------------------------------------------"
 }
 
 # Check if the user is root
 if [ "$USER_ID" -eq 0 ]; then
-    echo -e "${GREEN}SUCCESS: You are a root user. Script execution will start.${RESET}"
+    echo -e "${GREEN}SUCCESS: You are a root user. Script execution will start.${RESET}\n"
 else 
-    echo -e "${RED}ERROR: You are not a root user. Please switch to the root user.${RESET}"
+    echo -e "${RED}ERROR: You are not a root user. Please switch to the root user.${RESET}\n"
     exit 1
 fi
-echo
-echo "----------------------------------------------------------------------------------------"
+
+TASK_STARTED "Checking Python Installation"
 echo "CHECKING PYTHON INSTALLED OR NOT"
-if python3.6 --version
-then
-    echo -e "${RED}PYTHON ALREADY INSTALLED SO SKIPPING INSTALLATION"
+if python3.6 --version; then
+    echo -e "${RED}PYTHON ALREADY INSTALLED. SKIPPING INSTALLATION${RESET}\n"
 else
-    echo -e "${GREEN}PYTHON NOT INSTALLED SO STARTED INSTALLATION PART"
+    echo -e "${YELLOW}PYTHON NOT INSTALLED. STARTING INSTALLATION${RESET}\n"
     dnf install python36 gcc python3-devel -y &>>$LOG_FILE
     VALIDATE $? "PYTHON INSTALLATION"
-fi       
-echo "-------------------------------------------------------------------------------------------"
-echo
+fi
+
+TASK_STARTED "User Creation"
 id roboshop &>>$LOG_FILE
-if [ $? -eq 0 ]
-then
-    echo -e "${GREEN}ROBO-SHOP USER ALREADY AVAILABLE So SKIIPING USER CREATION ${GREEN}"
+if [ $? -eq 0 ]; then
+    echo -e "${YELLOW}ROBO-SHOP USER ALREADY AVAILABLE. SKIPPING USER CREATION${RESET}\n"
 else
-    echo -e "${GREEN}ROBO-SHOP USER CREATION STARTED"
+    echo -e "${YELLOW}ROBO-SHOP USER CREATION STARTED${RESET}\n"
     useradd roboshop &>>$LOG_FILE
     VALIDATE $? "ROBO-SHOP USER CREATION PART"
 fi
-echo "--------------------------------------------------------------------------------------------"
-echo
-if [ -d /app ]
-then
-    echo -e "{$RED}/app FOLDER ALREADY EXISTED SO SKIPPING FOLDER CREATION $RESET"
+
+TASK_STARTED "Creating App Folder"
+if [ -d /app ]; then
+    echo -e "${RED}/app FOLDER ALREADY EXISTS. SKIPPING FOLDER CREATION${RESET}\n"
 else
-    echo -e "${YELLOW}/app FOLDER CREATION STARTED $RESET"
+    echo -e "${YELLOW}/app FOLDER CREATION STARTED${RESET}\n"
     mkdir -p /app &>>$LOG_FILE
     VALIDATE $? "APP FOLDER CREATION"
 fi
-echo "-------------------------------------------------------------------------------------------"
-echo
-echo -e "${GREEN}DOWNLOADING THE APPLICATION CODE FROM INTERNET ${RESET}"
+
+TASK_STARTED "Downloading Application Code"
+echo -e "${YELLOW}DOWNLOADING THE APPLICATION CODE FROM INTERNET${RESET}\n"
 curl -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>>$LOG_FILE
-VALIDATE $? "APP CODE DOWALOADING"
-echo
-echo -e "${GREEN}UNZIPPING THE DOWNLOAD APP CODE"
+VALIDATE $? "APP CODE DOWNLOADING"
+
+TASK_STARTED "Unzipping Application Code"
+echo -e "${YELLOW}UNZIPPING THE DOWNLOADED APP CODE${RESET}\n"
 cd /app
 pwd
 unzip -o /tmp/payment.zip &>>$LOG_FILE
 VALIDATE $? "UNZIPPED CODE INTO /APP"
-echo
-echo -e "${GREEN}NPM INSTALLATION STARTED $RESET"
+
+TASK_STARTED "Installing Dependencies"
+echo -e "${YELLOW}PIP INSTALLATION STARTED${RESET}\n"
 pip3.6 install -r requirements.txt &>>$LOG_FILE
 VALIDATE $? "PIP 3.6 INSTALLATION"
-echo "-----------------------------------------------------------------------------------------"
 
-echo
+echo -e "-----------------------------------------------------------------------------------------\n"
+
+echo -e "${YELLOW}Copying payment.service to /etc/systemd/system${RESET}\n"
 cp /home/centos/shell-scripting-Roboshop-Automation/payment.service /etc/systemd/system/payment.service
-VALIDATE $? "Catalogue.service Copying"
-echo
+VALIDATE $? "Payment Service Copying"
+
+echo -e "${YELLOW}Reloading systemd daemon${RESET}\n"
 systemctl daemon-reload
-VALIDATE $? "DAEMON RELOADED"
+VALIDATE $? "Daemon Reloaded"
+
+echo -e "${YELLOW}Enabling payment service${RESET}\n"
 systemctl enable payment
-VALIDATE $? "ENABLED PAYMENT SERVICE"
-echo
+VALIDATE $? "Payment Service Enabled"
+
+echo -e "${YELLOW}Starting payment service${RESET}\n"
 systemctl start payment
-VALIDATE $? "STARTED PAYMENT SERVICE"
-echo "------------------------------ THE-END--------------------------------------"
-echo "SCRIPT END TIME: $0-$DATE"
+VALIDATE $? "Payment Service Started"
+echo -e "SCRIPT END TIME: $0-$DATE\n"
+echo -e "------------------------------ THE-END--------------------------------------\n"
+
