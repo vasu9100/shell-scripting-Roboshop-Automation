2f390e36094096b22397fe6145b5934ddc049c42
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-22 16:12:36 +0530

    robshop added

--- catalogue.sh
+++ catalogue.sh
@@ -29,7 +29,7 @@ VALIDATE() {
 
 # Function to print task started message
 TASK_STARTED() {
-    echo -e "${RED}Task Started: $1${RESET}"
+    echo -e "${PINK}Task Started: $1${RESET}"
 }
 
 # Check if the user is root
