1864291d3f3e3a85703f4b102f3c957708781a5e
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-23 11:53:15 +0530

    added new key

---
+++ ssh-key.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#!/bin/bash
+# Script Name: install_mongodb.sh
+# Purpose: This script installs MongoDB for the Roboshop application.
+# Author: Gonepudi Srinivas
+# Date: March 18, 2024
+# Version: 1.0
+
+# Define colors for better readability
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+PINK='\033[1;35m'
+RESET='\033[0m'
+
+# Log file setup
+DATE=$(date +'%F-%H-%M-%S')
+USER_ID=$(id -u)
+echo
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "${GREEN}SUCCESS: $2${RESET}"
+    else
+        echo -e "${RED}FAILED: $2${RESET}"
+        exit 1
+    fi
+}
+echo
+TASK_STARTED(){
+    echo "<--------------------------------------------------------------->"
+    echo -e "${PINK}TASK_STARTED--> $1 ${RESET}\n"
+    echo "<--------------------------------------------------------------->"
+
+}
+echo
+if [ "$USER_ID" -eq 0 ]; then
+    echo -e "${GREEN}SUCCESS: You are a root user. Script execution will start.${RESET}\n"
+else 
+    echo -e "${RED}ERROR: You are not a root user. Please switch to the root user.${RESET}\n"
+    exit 1
+fi
+echo
+TASK_STARTED "SSH KEY CREATION STARTED"
+if [ -f "/home/centos/.ssh/id_rsa.pub" ]
+then
+    echo -e "${RED}SSH PUBLIC KEY ALREADY EXISTED SO THIS $0 SCRIPT STOPPED CREATING NEW KEY"
+else
+    echo -e "${GREEN}SSH KEY IS NOT THERE SO THIS $0 SCRIPT STARTED CREATING NEW KEY/n"
+    ssh-keygen -t rsa -b 4096 -f id_rsa
+    VALIDATE $? "SSH KEY GENERATION"
+fi    
