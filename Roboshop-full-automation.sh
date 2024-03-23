cb2db48417e4b105cd53c8d693807bd0bd5c1f2c
srinivas gonepudi <srigonepudi@gmail.com>
2024-03-23 17:34:03 +0530

    added new file

--- Roboshop-full-automation.sh
+++ Roboshop-full-automation.sh
@@ -47,8 +47,8 @@ for name in $SERVER_NAMES;
 do
     TASK_STARTED "Executing script on $name"
     echo -e "${YELLOW}LOGGING: ${RESET}$name"
-    ssh -i /home/centos/.ssh/id_rsa centos@$name <<EOF
-    scp -i /home/centos/.ssh/id_rsa -r /home/centos/shell-scripting-Roboshop-Automation centos@$ip:/home/centos/
+    ssh centos@$name <<EOF
+    scp -r /home/centos/shell-scripting-Roboshop-Automation centos@$name:/home/centos/
 EOF
     VALIDATE $? "ALL SCRIPTS COPIED"
 done
